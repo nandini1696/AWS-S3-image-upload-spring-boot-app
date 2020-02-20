fa815c580b3d2dc68a8a6557110da67778696df4
nandini1696 <61177970+nandini1696@users.noreply.github.com>
2020-02-20 10:19:00 +0530

    Update AmazonClient.java

--- src/main/java/com/awsrecap/app/amazons3client/AmazonClient.java
+++ src/main/java/com/awsrecap/app/amazons3client/AmazonClient.java
@@ -1,4 +1,4 @@
-package com.awsrecap.app.amazons3client;
+
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -7,8 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
 
 @Configuration
 public class AmazonClient {
